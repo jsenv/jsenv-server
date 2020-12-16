52ce24e9444a3de0049f327f035ba005297a7e2a
Damien Maillard <damien.maillard@dailymotion.com>
2020-12-16 14:10:02 +0100

    test redirection

---
+++ test/http-redirection/http-polyglot.redirection.test.js
@@ -0,0 +1,62 @@
+import { assert } from "@jsenv/assert"
+import { jsenvPrivateKey, jsenvCertificate, fetchUrl } from "@jsenv/server"
+import { headersToObject } from "@jsenv/server/src/headersToObject.js"
+import { listen } from "@jsenv/server/src/internal/listen.js"
+import { createServer } from "./http-polyglot.js"
+
+const server = createServer({
+  privateKey: jsenvPrivateKey,
+  certificate: jsenvCertificate,
+  requestHandler: (nodeRequest, nodeResponse) => {
+    if (!nodeRequest.socket.encrypted) {
+      const host = nodeRequest.headers.host || nodeRequest.authority
+      nodeResponse.writeHead(301, { location: `https://${host}${nodeRequest.url}` })
+      nodeResponse.end()
+      return
+    }
+
+    nodeResponse.writeHead(200, { "content-Type": "text/plain" })
+    nodeResponse.end("Welcome, HTTPS user!")
+  },
+})
+server.unref()
+const port = await listen({
+  server,
+  port: 0,
+  ip: "127.0.0.1",
+})
+
+// 301 on http request
+{
+  const response = await fetchUrl(`http://127.0.0.1:${port}/file.js?page=2`, { redirect: "manual" })
+  const actual = {
+    status: response.status,
+    headers: headersToObject(response.headers),
+    body: await response.text(),
+  }
+  const expected = {
+    status: 301,
+    headers: {
+      "connection": "close",
+      "date": actual.headers.date,
+      "location": `https://127.0.0.1:${port}/file.js?page=2`,
+      "transfer-encoding": "chunked",
+    },
+    body: "",
+  }
+  assert({ actual, expected })
+}
+
+// 200 on https request
+{
+  const response = await fetchUrl(`https://127.0.0.1:${port}`, { ignoreHttpsError: true })
+  const actual = {
+    status: response.status,
+    body: await response.text(),
+  }
+  const expected = {
+    status: 200,
+    body: "Welcome, HTTPS user!",
+  }
+  assert({ actual, expected })
+}
